c51622c22adf50b1b85f68b4c1ed41dc325a6181
Victoria <bika0724@gmail.com>
2022-06-25 12:35:30 +0300

    Базовая типизация поля ввода

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -1,13 +1,28 @@
 import styles from './Input.module.css'
+import cn from "classnames";
 
-const Input = () => {
-    console.log(styles)
+interface InputProps {
+    value: string
+    onChange: any
+}
+
+const Input = (props: InputProps) => {
+
+    const {value, onChange} =props;
+
+    const placeholderClassName = cn(
+        [
+            styles.inputPlaceholder,
+            value.length && styles.inputPlaceholderModified
+        ]
+    )
+
     return (
         <div className={styles.inputWrapper}>
-            <div className={[styles.inputPlaceholder, styles.inputPlaceholderModified].join(' ')}>
+            <div className={placeholderClassName}>
                 Номер договора
             </div>
-            <input className={styles.input} type="text"/>
+            <input className={styles.input} type="text" value={value} onChange={onChange}/>
         </div>
     )
 }
--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,10 +1,19 @@
 import Layout from "@layouts/Layout/Layout";
 import Input from "@components/ui/Input/Input";
+import {useState} from "react";
 
 const NewsPage = () => {
+
+    const [userValue, setUserValue] =useState('')
+
+    const handleInputChange = (event)=> {
+        const {target} =event;
+        setUserValue(target.value)
+    }
+
     return (
         <Layout title="AOD Zone | test">
-           <Input/>
+           <Input value={userValue} onChange={handleInputChange}/>
         </Layout>
     )
 }
